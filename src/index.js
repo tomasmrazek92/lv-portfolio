da687395922985a32cf1cd55b798406718c594c8
Tomáš Mrázek <tomasmrazek92@gmail.com>
2025-10-01 12:49:12 +0200

    Fix player 3

--- src/index.js
+++ src/index.js
@@ -1031,16 +1031,13 @@ function initBunnyLightboxPlayer() {
 
     if (type === 'play' || type === 'pause' || type === 'playpause') {
       var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-      if (
-        isMobile &&
-        (video.paused || video.ended) &&
-        video.webkitSupportsFullscreen &&
-        typeof video.webkitEnterFullscreen === 'function'
-      ) {
-        video.webkitEnterFullscreen();
-      } else {
-        togglePlay();
+      if (isMobile && (video.paused || video.ended)) {
+        if (typeof video.webkitEnterFullscreen === 'function') {
+          video.webkitEnterFullscreen();
+          return;
+        }
       }
+      togglePlay();
     } else if (type === 'mute') {
       toggleMute();
     } else if (type === 'fullscreen') {
